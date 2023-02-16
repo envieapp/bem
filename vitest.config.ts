0a98eac376652e67d0421511c368e56118bc9d8e
BruhaBruh <drugsho.jaker@gmail.com>
2023-02-16 08:07:36 +0500

    ci: add coverage badge generation

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   plugins: [],
   test: {
     globals: true,
+    coverage: {
+      reporter: ['json-summary', 'text-summary'], // change this property to the desired output
+    },
   },
 });
